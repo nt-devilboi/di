dc495e41d014835be22fdd5d5babe8c3e174790f
devilboi95 <turnickii.n@gmail>
2024-12-18 13:28:54 +0500

    сделал CLI

--- TagCloud2/Program.cs
+++ TagCloud2/Program.cs
@@ -1,25 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-using System.Drawing;
-using Microsoft.Extensions.DependencyInjection;
 using TagCloud2;
-using TagsCloudVisualization;
 
-var serviceCollection = new ServiceCollection();
-serviceCollection.AddSingleton<SettingsTagCloud>(x =>
-    new SettingsTagCloud(new Size(2300, 2300), "./../../../photos"));
+var x = new TagCloudCli();
 
-
-serviceCollection.AddSingleton<ICloudLayouter, CircularCloudLayouter>(
-    x => new CircularCloudLayouter(new Point(11)));
-
-var loader = new FileWordLoader();
-var words = loader.LoadWord("./../../../Words.txt");
-Console.WriteLine(words[0].Word);
-
-var circularCloudLayouter = new CircularCloudLayouter(new Point(1150, 1150));
-using var tagImage = new CloudBitMap(2300, 2300, "./../../../photos");
-using var tagCloud = new TagCloud(circularCloudLayouter, tagImage);
-tagCloud.GenerateCloud(words, 50, 250);
-tagCloud.Save();
-Console.WriteLine("Hello, World!");
+x.Run();
